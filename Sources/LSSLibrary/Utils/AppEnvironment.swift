2382821439022fbd1a0edc7a5a8811b73d708424
Kyeongtae Nam <vituscokr@gmail.com>
2022-01-11 15:11:42 +0900

    AppEnvironment 선언

---
+++ Sources/LSSLibrary/Utils/AppEnvironment.swift
@@ -0,0 +1,14 @@
+//
+//  AppEnvironment
+//  앱 환경설정에 대한 enum 을 선언합니다.
+//
+//  Created by Gyeongtae Nam on 2022/01/11.
+//
+
+import Foundation
+
+enum AppEnvironmnet {
+    case develop
+    case stage
+    case release
+}
