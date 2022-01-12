d1c17bc4be4c35d0c3ca6f5d3f8bfafd5fa9006a
Kyeongtae Nam <vituscokr@gmail.com>
2022-01-12 13:40:14 +0900

    SimpleWebView 추가

---
+++ Sources/LSSLibrary/View/SimpleWebView.swift
@@ -0,0 +1,27 @@
+//
+//  SimpleWebView.swift
+//  Susuda
+//
+//  Created by Gyeongtae Nam on 2022/01/12.
+//
+
+import Foundation
+import UIKit
+import SwiftUI
+import WebKit
+
+public struct SimpleWebView: UIViewRepresentable {
+   @State var urlRequest : URLRequest?
+    
+    public func makeUIView(context: Context) -> some UIView {
+        let webView = WKWebView(frame:CGRect.zero)
+        guard let request = urlRequest else {
+            return webView
+        }
+        webView.load(request)
+        return webView
+    }
+    public func updateUIView(_ uiView: UIViewType, context: Context) {
+        
+    }
+}
