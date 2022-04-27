fe6489a60e829ad094c27127b7ace177d6248d2c
Kyeongtae Nam <vituscokr@gmail.com>
2022-04-27 13:34:13 +0900

    public

--- Sources/LSSLibrary/Enums/RequestError.swift
+++ Sources/LSSLibrary/Enums/RequestError.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-enum RequestError :Error {
+public enum RequestError :Error {
     case requestNoSuccess
     case requestError(message:String)
     case requestFailError(error:Error)
