a6f1f61fa9efb2412b61a65861f236ea14a85494
Kyeongtae Nam <vituscokr@gmail.com>
2022-04-27 13:27:51 +0900

    RequestError 추가

---
+++ Sources/LSSLibrary/Enums/RequestError.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Gyeongtae Nam on 2022/04/27.
+//
+
+import Foundation
+
+enum RequestError :Error {
+    case requestNoSuccess
+    case requestError(message:String)
+    case requestFailError(error:Error)
+}
