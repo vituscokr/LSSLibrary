c8637fca8fb22b5bcb1fd2594e0cd0b793b33fc2
vitus <vituscokr@gmail.com>
2022-05-03 09:30:43 +0900

    ㅜㅜ

---
+++ Sources/LSSLibrary/Representable/PhotoPickerViewDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  
+//
+//  Created by vitus on 2022/05/03.
+//
+
+import Foundation
+import UIKit
+import TLPhotoPicker
+
+public class PhotoPickerViewDelegate : UIViewController , UINavigationControllerDelegate {
+    
+    public init() {
+        
+    }
+}
+
+
+//extension PhotoPickerViewDelegate : TLPhotosPickerViewControllerDelegate {
+//    
+//}
