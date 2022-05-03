927f88ed1eded7d5700e0290dc71dbffb5ad5802
vitus <vituscokr@gmail.com>
2022-05-03 09:49:48 +0900

    super.init

--- Sources/LSSLibrary/Representable/PhotoPickerViewDelegate.swift
+++ Sources/LSSLibrary/Representable/PhotoPickerViewDelegate.swift
@@ -11,10 +11,10 @@ import TLPhotoPicker
 
 public class PhotoPickerViewDelegate : UIViewController , UINavigationControllerDelegate {
     public init() {
-        
+        super.init(nibName: nil, bundle: nil)
     }
     public required init?(coder: NSCoder) {
-        
+        super.init(nibName: nil, bundle: nil)
     }
 }
 
